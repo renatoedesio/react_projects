bdca7c7d8e5b95a4901a8ad127f1baba3fe4baa8
Renato Edesio <renatoedesio@gmail.com>
2023-06-09 17:09:18 -0300

    Arrumando pequenos bugs no navItems

--- src/layouts/navItems/NavItems.jsx
+++ src/layouts/navItems/NavItems.jsx
@@ -24,17 +24,18 @@ export default function NavItem({icon, title, description, active, navSize, goTo
                 <NavLink to={goTo}>
                 <Link
                     backgroundColor={active && "#AEC8CA"}
-                    p={navSize == "small" ? "1" : "3"}
+                    p={navSize == "small" ? "0" : "3"} 
                     borderRadius={8}
                     _hover={{ textDecor: 'none', backgroundColor: "#AEC8CA" }}
-                    w={navSize == "large" && "100%"}
-                >
+                    w={navSize == "large" && "100%"}> 
+              
                     <MenuButton w="100%">
-                        <Flex alignItems={'center'}>
+                        <Flex justifyContent={'center'}>
                             <Icon as={icon} fontSize="xl" color={active ? "#82AAAD" : "gray.500"} />
                             <Text ml={5} display={navSize == "small" ? "none" : "flex"}>{title}</Text>
                         </Flex>
                     </MenuButton>
+                 
                 </Link>
                 </NavLink>
 
