9b888b500b8ca60b91276efdd4dd8e134c761a1f
renatoedesio <renatoedesio@unb.br>
2023-06-14 18:46:32 -0300

    Corrigindo bug da autenticacao

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
     <ChakraProvider >    
       <App />
     </ChakraProvider>
-  </React.StrictMode>
+  
 )
